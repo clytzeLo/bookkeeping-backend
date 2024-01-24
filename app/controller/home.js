8b2faf878e57c7d3bab2e5ae6a29d106a3c2449d
clytze <clytzelo@163.com>
2024-01-24 16:38:36 +0800

    feat: 测试post请求方式

--- app/controller/home.js
+++ app/controller/home.js
@@ -10,6 +10,13 @@ class HomeController extends Controller {
     const { id } = ctx.params
     ctx.body = `hi user = ${id}`
   }
+  async addUser() {
+    const { ctx } = this;
+    const { title } = ctx.request.body;
+    ctx.body = {
+      title
+    }
+  }
 }
 
 module.exports = HomeController;
